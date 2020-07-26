5651a0fd4d2064296ced70c7fcf9704668b1efdd
Dmitry Rakovets <dmitryrakovets@gmail.com>
2020-07-26 23:32:24 +0300

    feat: add theme 'Bean Lifecycle'

---
+++ course-java-spring-bean-lifecycle/src/main/java/com/rakovets/course/java/spring/example/beanlifecycle/annotation/Language.java
@@ -0,0 +1,6 @@
+package com.rakovets.course.java.spring.example.beanlifecycle.annotation;
+
+
+public enum Language {
+    RU, EN
+}
---
+++ course-java-spring-bean-lifecycle/src/main/java/com/rakovets/course/java/spring/example/beanlifecycle/service/HeroService.java
@@ -0,0 +1,6 @@
+package com.rakovets.course.java.spring.example.beanlifecycle.service;
+
+
+public interface HeroService {
+    void printHeroNameById(Long id);
+}

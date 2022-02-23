dfa60f5f27f00252308660897fde9fad4ee94de4
Dmitry Rakovets <dmitryrakovets@gmail.com>
2022-02-23 15:04:35 +0300

    refactor: update theme 'Spring Framework: Properties'

--- spring-framework/properties/src/main/java/by/rakovets/course/java/spring/framework/example/properties/UserDaoProd.java
+++ spring-framework/properties/src/main/java/by/rakovets/course/java/spring/framework/example/properties/UserDaoProd.java
@@ -4,16 +4,21 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Repository;
 
+import java.util.Properties;
+
 
 @Repository
 @Profile("prod")
 public class UserDaoProd implements UserDao {
     @Value("${jdbc.url}")
     private String url;
+    @Value("${jdbc.password}")
+    private String password;
 
     @Override
     public String getNameById(Long id) {
         System.out.println(url);
+        System.out.println(password);
         System.out.println(this.getClass().getSimpleName());
         return this.getClass().getSimpleName();
     }
